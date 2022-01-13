6e03a24ba5f2fd2f8637fa00582a0c43cb6e172f
Evan Danaher <github@edanaher.net>
2022-01-12 21:16:06 -0500

    Add some qwerty and SimpleChords to Splitography.

--- examples/Devices/SOFTHRUF/Splitography/Splitography.ino
+++ examples/Devices/SOFTHRUF/Splitography/Splitography.ino
@@ -18,14 +18,24 @@
  */
 
 #include "Kaleidoscope.h"
+#include "Kaleidoscope-FocusSerial.h"
 #include "Kaleidoscope-Steno.h"
+#include "Kaleidoscope-SimpleChords.h"
 
 enum {
+  _QWERTY,
   _STENO,
 };
 
 /* *INDENT-OFF* */
 KEYMAPS(
+
+ [_QWERTY] = KEYMAP(
+    Key_Tab,       Key_Q,    Key_W,    Key_E,    Key_R,    Key_T,    Key_Y,    Key_U,    Key_I,    Key_O,    Key_P,    Key_Backspace,
+    Key_Escape,    Key_A,    Key_S,    Key_D,    Key_F,    Key_G,    Key_H,    Key_J,    Key_K,    Key_L,    Key_Semicolon, Key_Quote,
+    Key_LeftShift, Key_Z,    Key_X,    Key_C,    Key_V,    Key_B,    Key_N,    Key_M,    Key_Comma, Key_Period,  Key_Slash, Key_Enter ,
+                                                MoveToLayer(_STENO), Key_Space    , Key_LeftControl   , Key_LeftShift
+  ),
 
 /* Steno (GeminiPR)
  * ,-----------------------.      ,-----------------------.
@@ -41,13 +51,13 @@ KEYMAPS(
  [_STENO] = KEYMAP(
     S(N1) ,S(N2) ,S(N3) ,S(N4) ,S(N5) ,S(N6)    ,S(N7)  ,S(N8) ,S(N9) ,S(NA) ,S(NB) ,S(NC)
    ,XXX   ,S(S1) ,S(TL) ,S(PL) ,S(HL) ,S(ST1)   ,S(ST3) ,S(FR) ,S(PR) ,S(LR) ,S(TR) ,S(DR)
-   ,XXX   ,S(S2) ,S(KL) ,S(WL) ,S(RL) ,S(ST2)   ,S(ST4) ,S(RR) ,S(BR) ,S(GR) ,S(SR) ,S(ZR)
+   ,MoveToLayer(_QWERTY)   ,S(S2) ,S(KL) ,S(WL) ,S(RL) ,S(ST2)   ,S(ST4) ,S(RR) ,S(BR) ,S(GR) ,S(SR) ,S(ZR)
                                ,S(A)  ,S(O)     ,S(E)   ,S(U)
   )
 );
 /* *INDENT-ON* */
 
-KALEIDOSCOPE_INIT_PLUGINS(GeminiPR);
+KALEIDOSCOPE_INIT_PLUGINS(Focus, GeminiPR, SimpleChords);
 
 void setup() {
   Kaleidoscope.serialPort().begin(9600);
